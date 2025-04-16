1a9dca6525efaf785cb1b7850a7b848baafa83c3
Saravanan <saravanan2003cr7@gmail.com>
2025-04-16 19:05:57 +0530

    final commit

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -30,7 +30,7 @@ export const Footer = () => {
           <SocialYoutube />
         </div>
         <p className="mt-6">&copy; 2024 Your Company, Inc. All rights reserved.
-          <br /><Link className="hover:text-white transition-all" href="https://github.com/MiladJoodi/Light-Saas-Landing-Page">Make with ❤️ by Joodi</Link>
+        
         </p>
       </div>
     </footer>
